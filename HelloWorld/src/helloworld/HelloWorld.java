c62fab59775ad3ff18febb56fc215294a45e7ebf
admin <admin@Win7_1TB>
2018-10-29 09:21:45 +0100

    He añadido display

--- HelloWorld/src/helloworld/HelloWorld.java
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -16,7 +16,7 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola mundo");
+        System.out.println("Hola mundo ");
     }
     
 }
